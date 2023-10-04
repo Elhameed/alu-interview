8f9721828b38a5d366e693e98a5bfce92c176ab7
Abdulhameed Teniola Ajani <a.ajani@alustudent.com>
2023-10-04 12:12:50 +0100

    Update 0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
+"""Calculate how many square units of water will be retained after it rains."""
+
 
 def rain(walls):
-
     """
-    Calculate the total amount of rainwater retained between walls.
+    Calculate the amount of water retained after it rains.
 
     Args:
-        walls (List[int]): A list of non-negative integers representing the heights of walls.
+        walls (list): non-negative integers .
 
     Returns:
-        int: The total amount of rainwater retained.
-
-    Note:
-        - If the input list is empty, the function returns 0.
-        - The ends of the list (before index 0 and after index walls[-1]) are not considered walls.
-
-    Example:
-        >>> walls = [0, 1, 0, 2, 0, 3, 0, 4]
-        >>> rain(walls)
-        6
+        int: The total number of square units of water retained.
 
     """
-  
+
     if not walls:
         return 0
 
@@ -29,24 +21,19 @@ def rain(walls):
     left_max = [0] * n
     right_max = [0] * n
 
+    # Calculate the maximum height to the left of each wall
     left_max[0] = walls[0]
     for i in range(1, n):
-        left_max[i] = max(left_max[i - 1], walls[i])
+        left_max[i] = max(left_max[i-1], walls[i])
 
-    right_max[n - 1] = walls[n - 1]
-    for i in range(n - 2, -1, -1):
-        right_max[i] = max(right_max[i + 1], walls[i])
+    # Calculate the maximum height to the right of each wall
+    right_max[n-1] = walls[n-1]
+    for i in range(n-2, -1, -1):
+        right_max[i] = max(right_max[i+1], walls[i])
 
-    total_water = 0
+    water_units = 0
+    # Calculate the amount of water retained above each wall
     for i in range(n):
-        total_water += min(left_max[i], right_max[i]) - walls[i]
+        water_units += min(left_max[i], right_max[i]) - walls[i]
 
-    return total_water
-
-# Test cases
-if __name__ == "__main__":
-    walls1 = [0, 1, 0, 2, 0, 3, 0, 4]
-    print(rain(walls1))  # Output: 6
-
-    walls2 = [2, 0, 0, 4, 0, 0, 1, 0]
-    print(rain(walls2))  # Output: 6
+    return water_units
